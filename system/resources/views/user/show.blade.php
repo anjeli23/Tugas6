402249e7c60506c7353ef9a42dfa93016f6042f5
anjeli23 <sentianaanjeli041@gmail.com>
2022-09-10 21:46:26 +0700

    Tugas6
    Mengerjakan tugas 6

---
+++ system/resources/views/user/show.blade.php
@@ -0,0 +1,22 @@
+@extends('template2.base')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mt-4">
+                <div class="card">
+                    <div class="card-header">
+                        Detail Data User
+                    </div>
+                    <div class="card-body">
+                        <h4>{{ $user->nama }}</h4>
+                        <hr>
+                        {{ '@' . $user->username }} |
+                        Email : {{ $user->email }} |
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
